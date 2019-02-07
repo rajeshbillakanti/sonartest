5be394855fb22090c26300b953c8d3ad7315fa23
Rajesh Billakanti <rbillakanti@liquidhub.com>
2019-02-07 20:00:18 +0530

    first commit

---
+++ src/main/java/com/rajesh/sonar/MavenSonar/App.java
@@ -0,0 +1,13 @@
+package com.rajesh.sonar.MavenSonar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
